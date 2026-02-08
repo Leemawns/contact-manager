62948bc069295fe8f5b2b654f07a6d0fb46e0372
Leemawns <eric.lemons.dev@gmail.com>
2026-02-08 15:57:35 -0500

    Create SearchContacts API
    Initial creation of the search (read) API. Currently only reads in the input json file and parses it for the necessary info as well as initializing the variables to be used in search list creation.

---
+++ LAMPAPI/SearchContacts.php
@@ -0,0 +1,24 @@
+<?php
+    $inData = getRequestInfo();
+
+    if(!isset($inData["userId"], $inData["search"])){
+        returnWithError("Missing required field(s).");
+        exit()
+    }
+
+    $userId = (int)$inData["userId"];
+    $search = $inData["search"];
+    $searchResults = ""
+    $searchCount = 0;
+
+    if($userId <= 0){
+        returnWIthError("Invalid input.");
+        exit();
+    }
+
+    $conn = new mysqli("localhost", "contact_api", "1JERKApassword", "contact_manager");
+
+    
+
+
+?>
